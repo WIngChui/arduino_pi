79929616a186b5642485d08be5a8a4c6ca288f57
WingChui <wingchui1226@gmail.com>
2018-01-18 20:29:15 +0800

    test template

--- arduino_pi.py
+++ arduino_pi.py
@@ -4,18 +4,22 @@ import os
 from arduino_function import *
 
 filename = 'record.csv'
+time_interval = 30	#	seconds
+#loop_timer = RepeatingTimer(time_interval, loop_get)
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-	return render_template('index.html', file=read_csv(filename))
-	
-loop_timer = RepeatingTimer(5*60, loop_get)
+	record_list = read_csv()
+	temp = [i[0] for i in record_list]
+	humidity = [i[1] for i in record_list]
+	light_resist = [i[2] for i in record_list]
+	return render_template('index.html', time_interval = time_interval*1000, temp = temp, humidity = humidity, light_resist = light_resist)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
-	global loop_timer.start()
+	#global loop_timer.start()
 	app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug=True)
 	
 	
